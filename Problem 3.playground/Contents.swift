eb05f023c5112f572c60e709e24e922fb4b7a29e
Michael Metzger <metzgerenator@gmail.com>
2016-02-21 22:16:49 -0500

    finished problem 3

--- Problem 3.playground/Contents.swift
+++ Problem 3.playground/Contents.swift
@@ -1,28 +1,53 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
+//import UIKit
 
 //
 //*/The prime factors of 13195 are 5, 7, 13 and 29.
 //
 //What is the largest prime factor of the number 600851475143 ? *\
 
+//use this array as a base
+var factors = [2,3,5,7,11,13]
 
-let factors = [2,3,5,7,11,13]
+//This will be the array of odd number that we use to find the answer
+var factorsTwo = [2,3,5,7,11,13]
 
+
+//Start from 13 and add odd number until 2000
+var factorIncreaser = 2000
+var numberInput = 13
+
+while numberInput < factorIncreaser {
+    
+   
+    if (numberInput % 2 != 0 && numberInput % 3 != 0 &&  numberInput % 5 != 0 &&  numberInput % 7 != 0 &&  numberInput % 11 != 0){
+        
+        factorsTwo.append(numberInput)
+        
+    }
+    numberInput++
+}
+
+
+//Store the factors in this array
 var factorStorage:[Int] = []
 
 
-let inputNumber = 13195
+var inputNumber = 600851475143
 
-
-for numbers in factors {
+//Check through large array until Input number cannot be divided anymore
+for var x = 0; x < factorsTwo.count ;x++ {
+    let number = factorsTwo[x]
+    let factor  = inputNumber % number
     
-    let factor  = inputNumber % numbers
     
-    if (factor != 0){
+    if (factor == 0){
         
-     factorStorage.append(factor)
+        factorStorage.append(number)
+        //print(number)
+        
+        inputNumber = inputNumber / number
+        
+      x = 0
         
     }
     
@@ -30,10 +55,13 @@ for numbers in factors {
     
     
     
+    
 }
 
+print("The answer is \(inputNumber)")
 
-print(factorStorage)
+
+
 
 
 
